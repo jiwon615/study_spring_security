d4e984b557bea2899aa0d78e4f2f7187a23503e7
jiwon615 <seg615@naver.com>
2023-04-03 22:04:04 +0900

    feat: formLogin 방식 API 학습

--- src/main/java/com/study/security/config/SecurityConfig.java
+++ src/main/java/com/study/security/config/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.study.security.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -17,7 +28,28 @@ public class SecurityConfig {
                 .anyRequest().authenticated(); // 현재는 어떤 요청에도 인증 받도록 설정
 
         http
-                .formLogin();
+                .formLogin()
+                .loginPage("/loginPage")  // 직접 정의한 로그인 페이지로 이동
+                .defaultSuccessUrl("/")  // successHandler() 없는 경우, 이 defaultSuccessUrl이 동작
+                .failureUrl("/login")
+                .usernameParameter("userId") // default 는 username
+                .passwordParameter("passwd") // default 는 password
+                .loginProcessingUrl("/login_proc")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        log.info("authentication name={}", authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        log.info("exception message={}", exception.getMessage());  // exception message=자격 증명에 실패하였습니다.
+                        response.sendRedirect("/login");
+                    }
+                })
+                .permitAll(); // .loginPage("/loginPage") 접근하는 경우 인증 받지 않도록 함
         return http.build();
     }
 }
---
+++ src/main/java/com/study/security/controller/SecurityController.java
@@ -0,0 +1,20 @@
+package com.study.security.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+}
