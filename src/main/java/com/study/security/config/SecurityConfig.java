c57233534242b585a4fe657a1f1608ca63f0e026
jiwon615 <seg615@naver.com>
2023-04-04 19:20:13 +0900

    feat: 동시세션 제어 API

--- src/main/java/com/study/security/config/SecurityConfig.java
+++ src/main/java/com/study/security/config/SecurityConfig.java
@@ -34,30 +34,31 @@ public class SecurityConfig {
                 .authorizeRequests()  // 요청에 대한 보안 검사를 하겠다
                 .anyRequest().authenticated(); // 현재는 어떤 요청에도 인증 받도록 설정
 
-        http
-                .formLogin()
+        // FormLogin API
+        http.formLogin()
 //                .loginPage("/loginPage")  // 직접 정의한 로그인 페이지로 이동
                 .defaultSuccessUrl("/")  // successHandler() 없는 경우, 이 defaultSuccessUrl이 동작
                 .failureUrl("/login")
                 .usernameParameter("userId") // default 는 username
                 .passwordParameter("passwd") // default 는 password
                 .loginProcessingUrl("/login_proc")
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        log.info("authentication name={}", authentication.getName());
-                        response.sendRedirect("/");
-                    }
-                })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                        log.info("exception message={}", exception.getMessage());  // exception message=자격 증명에 실패하였습니다.
-                        response.sendRedirect("/login");
-                    }
-                })
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        log.info("authentication name={}", authentication.getName());
+//                        response.sendRedirect("/");
+//                    }
+//                })
+//                .failureHandler(new AuthenticationFailureHandler() {
+//                    @Override
+//                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//                        log.info("exception message={}", exception.getMessage());  // exception message=자격 증명에 실패하였습니다.
+//                        response.sendRedirect("/login");
+//                    }
+//                })
                 .permitAll(); // .loginPage("/loginPage") 접근하는 경우 인증 받지 않도록 함
 
+        // Logout API
         http.logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("")  // 이동할 페이지
@@ -75,12 +76,18 @@ public class SecurityConfig {
                         response.sendRedirect("/login");
                     }
                 })
-                .and()
-                .rememberMe()
+
+                // REMEMBER ME API
+                .and().rememberMe()
                 .rememberMeParameter("remember")
                 .tokenValiditySeconds(3600)
-                .userDetailsService(userDetailsService)
-        ;
+                .userDetailsService(userDetailsService);
+
+        // 동시세션 제어 API
+        http.sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false);
+
         return http.build();
     }
 }
