b0940b21ffc15ae27e38af2cad012eb7f32ea17b
jiwon615 <seg615@naver.com>
2023-04-05 21:43:03 +0900

    feat: 다중 보안 설정 V2

--- src/main/java/com/study/security/SecurityApplication.java
+++ src/main/java/com/study/security/SecurityApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.study.security.config.v1")
+//@ComponentScan("com.study.security.config.v1")
+@ComponentScan("com.study.security.config.v2")
 @SpringBootApplication(scanBasePackages = "com.study.security.controller")
 public class SecurityApplication {
 
