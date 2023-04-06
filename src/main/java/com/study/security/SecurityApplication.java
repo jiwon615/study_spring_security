3e33cf9fd2b8d2eb22c8b36982751658e94e1552
jiwon615 <seg615@naver.com>
2023-04-06 19:50:35 +0900

    feat: v3 config3

--- src/main/java/com/study/security/SecurityApplication.java
+++ src/main/java/com/study/security/SecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 //@ComponentScan("com.study.security.config.v1")
-@ComponentScan(basePackages = {"com.study.security.config.v2", "com.study.security.controller"})
+@ComponentScan(basePackages = {"com.study.security.config.v3", "com.study.security.controller"})
 @SpringBootApplication
 public class SecurityApplication {
 
