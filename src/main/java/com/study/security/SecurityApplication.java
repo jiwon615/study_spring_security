9806f47de5ea0cee93e386297a19778d8e019592
jiwon615 <seg615@naver.com>
2023-04-07 13:47:33 +0900

    comment: 주석 추가

--- src/main/java/com/study/security/SecurityApplication.java
+++ src/main/java/com/study/security/SecurityApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan("com.study.security.config.v1")
+//@ComponentScan(basePackages = {"com.study.security.config.v1", "com.study.security.controller"})
+//@ComponentScan(basePackages = {"com.study.security.config.v2", "com.study.security.controller"})
 @ComponentScan(basePackages = {"com.study.security.config.v3", "com.study.security.controller"})
 @SpringBootApplication
 public class SecurityApplication {
