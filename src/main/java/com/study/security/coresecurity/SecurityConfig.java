ba75aa21160cb334b9b0171f69aae24a6a113a3a
jiwon615 <seg615@naver.com>
2023-04-07 14:36:56 +0900

    feat: 기본 securityConfig 세팅 및 정적자원 관리 설정

--- src/main/java/com/study/security/coresecurity/SecurityConfig.java
+++ src/main/java/com/study/security/coresecurity/SecurityConfig.java
@@ -1,17 +1,71 @@
 package com.study.security.coresecurity;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    // 기본 사용자 추가 등록
+    @Bean
+    public UserDetailsManager users() {
+        String password = passwordEncoder().encode("1111");
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(password)
+                .roles("USER")
+                .build();
+
+        UserDetails manager = User.builder()
+                .username("manager")
+                .password(password)
+                .roles("MANAGER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(password)
+                .roles("ADMIN", "MANAGER", "USER")
+                .build();
+        return new InMemoryUserDetailsManager( user, manager, admin ); // 총 3명의 사용자 생성!
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // 정적파일들은 포안필터 거치지 않고 통과되도록 함 (permitAll과 다른점은 보안필터를 거치고, 통과시키는것)
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 인가 처리
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/mypage").hasRole("USER")
+                .antMatchers("/messages").hasRole("MANAGER")
+                .antMatchers("/config").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http
+                .formLogin();
         return http.build();
     }
 }
