6c81c0d0f7bd732a3f9342cd051d895e67ab0591
jiwon615 <seg615@naver.com>
2023-04-07 13:53:26 +0900

    master branch 내용 제거

--- src/main/java/com/study/security/SecurityApplication.java
+++ src/main/java/com/study/security/SecurityApplication.java
@@ -2,10 +2,7 @@ package com.study.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan("com.study.security.config.v1")
-@ComponentScan(basePackages = {"com.study.security.config.v3", "com.study.security.controller"})
 @SpringBootApplication
 public class SecurityApplication {
 
